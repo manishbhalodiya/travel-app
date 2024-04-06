b18857c44367caf9ced39dc625fa8a7cc2429774
manishbhalodiya <64132850+manishbhalodiya@users.noreply.github.com>
2024-04-06 05:45:36 +0000

    Updated bgImage style

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -17,9 +17,10 @@ const CampSite = ({
 }: CampSiteProps) => {
     return (
         <div
-            className={`h-full w-full min-w-[1100px] ${bgImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+            className={`h-full w-full min-w-[1100px] bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`} style={{ backgroundImage: 'url(' + bgImage + ')'}}
         >
-            <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+            {/* <Image src={bgImage} className="h-full w-full min-w-[1100px] bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl z-0" alt={bgImage} width={1100} height={500} /> */}
+            <div className="flex relative h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 z-1">
                 <div className="flexCenter gap-4">
                     <div className="rounded-full bg-green-50 p-4">
                         <Image
@@ -61,13 +62,13 @@ const Camp = () => {
             <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
                 <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
                     <CampSite
-                        bgImage="bg-img-1"
+                        bgImage="img-1.png"
                         title="Putuk Truno Camp"
                         subtitle="Prigen, Pasuruan"
                         peopleJoined="50+ Joined"
                     />
                     <CampSite
-                        bgImage="bg-bg-img-2"
+                        bgImage="img-2.png"
                         title="Mountain View Camp"
                         subtitle="Somewhere in the Wilderness"
                         peopleJoined="50+ Joined"
